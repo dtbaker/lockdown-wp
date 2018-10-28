74a724ff45fba916f3f410a0f31028341456aa7f
dtbaker <dtbaker@gmail.com>
2018-10-28 14:27:34 +1000

    init

--- views/sections/settings.php
+++ views/sections/settings.php
@@ -17,19 +17,38 @@ $lockdown_options = $lockdown->get_lockdown_options();
 	<?php foreach ( $lockdown_options as $lockdown_option => $lockdown_settings ) { ?>
 		<div class="lockdown-wp__option">
 			<div class="lockdown-wp__option-setting">
-				<input type="checkbox" name="lockdown_wp[<?php echo esc_attr( $lockdown_option ); ?>]" value="1"
-				       class="lockdown-wp__option-check"
-				       id="lockdown-wp__option-<?php echo esc_attr( $lockdown_option ); ?>"
-					<?php echo ! empty( $lockdown_settings['enabled'] ) ? ' checked' : ''; ?>>
-				<label for="lockdown-wp__option-<?php echo esc_attr( $lockdown_option ); ?>"
-				       class="lockdown-wp__option-label">
+				<div class="lockdown-wp__option-checkwrap">
+					<input type="checkbox" name="lockdown_wp[<?php echo esc_attr( $lockdown_option ); ?>]" value="1"
+					       class="lockdown-wp__option-check"
+					       id="lockdown-wp__option-<?php echo esc_attr( $lockdown_option ); ?>"
+						<?php echo ! empty( $lockdown_settings['enabled'] ) ? ' checked' : ''; ?>>
+					<label for="lockdown-wp__option-<?php echo esc_attr( $lockdown_option ); ?>"
+					       class="lockdown-wp__option-labelcheck">
+						<svg viewBox="0,0,50,50">
+							<path d="M5 30 L 20 45 L 45 5"></path>
+						</svg>
+					</label>
+				</div>
+				<label class="lockdown-wp__option-labeltext"
+				       for="lockdown-wp__option-<?php echo esc_attr( $lockdown_option ); ?>">
 					<?php echo esc_html( $lockdown_settings['title'] ); ?>
 				</label>
-			</div>
-			<div class="lockdown-wp__option-meta">
-				<div class="lockdown-wp__option-description">
+				<div class="lockdown-wp__option-labeldescription">
 					<p><?php echo esc_html( $lockdown_settings['description'] ); ?></p>
 				</div>
+				<div class="lockdown-wp__option-toggle">
+					<p>
+						<label for="lockdown-wp__option-meta-<?php echo esc_attr( $lockdown_option ); ?>"
+						       class="lockdown-wp__option-toggle-label">
+							Pro's &amp; Con's &raquo;
+						</label>
+					</p>
+				</div>
+			</div>
+			<input type="checkbox" name="toggler" value="1"
+			       class="lockdown-wp__option-toggler"
+			       id="lockdown-wp__option-meta-<?php echo esc_attr( $lockdown_option ); ?>">
+			<div class="lockdown-wp__option-meta">
 				<div class="lockdown-wp__option-feature">
 					<div class="lockdown-wp__option-feature--pro">
 						<h4><?php esc_html_e( 'Pros:', 'lockdown-wp' ); ?></h4>
@@ -44,5 +63,6 @@ $lockdown_options = $lockdown->get_lockdown_options();
 		</div>
 	<?php } ?>
 
-	<input type="submit" name="upload" value="<?php esc_attr_e( 'Save Settings', 'lockdown-wp' ); ?>">
+	<input type="submit" name="upload" value="<?php esc_attr_e( 'Save Settings', 'lockdown-wp' ); ?>"
+	       class="button button-primary">
 </form>
